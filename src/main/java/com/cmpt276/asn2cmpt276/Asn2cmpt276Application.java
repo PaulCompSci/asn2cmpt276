ebc526a7900fb793a0c2fe0150f00fe8ef65a9dc
paul <kohleangpaul911@gmail.com>
2023-06-05 11:20:25 -0700

    first commit

---
+++ src/main/java/com/cmpt276/asn2cmpt276/Asn2cmpt276Application.java
@@ -0,0 +1,13 @@
+package com.cmpt276.asn2cmpt276;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Asn2cmpt276Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Asn2cmpt276Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/cmpt276/asn2cmpt276/Asn2cmpt276ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmpt276.asn2cmpt276;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Asn2cmpt276ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
