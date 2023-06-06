0becea42661dd8a397030b9411f5cf18552b59c9
paul <kohleangpaul911@gmail.com>
2023-06-06 09:31:24 -0700

    add student page frontend done

---
+++ src/main/resources/static/javascript/addStudent.js
@@ -0,0 +1,8 @@
+
+//display current gpa for gpa slider 
+
+    var get = document.getElementById("gpa") ;
+    get.oninput= (()=>{
+        value = get.value ; 
+        document.getElementById("currGPA").textContent = value  ;
+    });
